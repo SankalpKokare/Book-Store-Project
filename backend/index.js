49f92acca343b49ffc964e273fd8883e6feef6e3
Sankalp Kokare <sankalpkokare21@gmail.com>
2024-03-31 18:14:11 +0530

    save book to database

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,43 @@
-import express from "express";
+import express, { response } from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from './models/bookModel.js'
 
 const app = express();
+
+//Middleware for parsing request body
+//change content-type=application/json
+app.use(express.json());
 
 app.get('/', (req, res) => {
     console.log(req);
     return res.status(234).send('Welcome to MERN Stack tutorial');
 })
 
+//Route to save a new book
+app.post('/books', async (req, res) => {
+    try {
+        if (!req.body.title || !req.body.author || !req.body.publishYear) {
+            return res.status(400).send({
+                message: 'Send all required fields: title, author, publishYear'
+            });
+        }
+        const newBook = {
+            title: req.body.title,
+            author: req.body.author,
+            publishYear: req.body.publishYear,
+        }
+
+        //save book
+        const book = await Book.create(newBook);
+
+        return res.status(201).send(book);
+
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({ message: error.message })
+    }
+})
 
 mongoose
     .connect(mongoDBURL)
