1aea9c24ecc1e4565c75cb9d35f4c6be900ec8b2
Sankalp Kokare <sankalpkokare21@gmail.com>
2024-03-31 18:26:48 +0530

    route for get book by id

--- backend/index.js
+++ backend/index.js
@@ -56,6 +56,21 @@ app.get('/books', async (req, res) => {
     }
 })
 
+//get book by id 
+app.get('/books/:id', async (req, res) => {
+    try {
+
+        const { id } = req.params;
+        const book = await Book.findById(id);
+        //return res.status(200).json(books);
+        return res.status(200).json(book);
+
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+})
+
 mongoose
     .connect(mongoDBURL)
     .then(() => {
