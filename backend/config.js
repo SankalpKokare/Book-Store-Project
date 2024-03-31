e20cb530cbcfffceeb19e111890ce1dee83ed579
Sankalp Kokare <sankalpkokare21@gmail.com>
2024-03-31 17:29:42 +0530

    connected to mongodb

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,3 @@
-const PORT = 5555;
-export default PORT;
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://root:root@book-store-mern.xwoyypk.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-Store-MERN';
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,23 @@
 import express from "express";
-import PORT from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
+
+app.get('/', (req, res) => {
+    console.log(req);
+    return res.status(234).send('Welcome to MERN Stack tutorial');
+})
 
 app.listen(PORT, () => {
     console.log(`App is listening to ${PORT}`)
 });
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('App connected to database');
+    })
+    .catch((error) => {
+        console.log(error);
+    })
