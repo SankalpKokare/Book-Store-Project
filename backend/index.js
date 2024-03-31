2cc4d100674c4f7f561b6834a553828d59e03fa8
Sankalp Kokare <sankalpkokare21@gmail.com>
2024-03-31 19:03:40 +0530

    updated cors policy in node js and express

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,26 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
-import { Book } from './models/bookModel.js'
 import booksRoute from './Routes/booksRoute.js';
+import cors from 'cors'
 
 const app = express();
 
 //Middleware for parsing request body
 //change content-type=application/json
 app.use(express.json());
+
+//method 1
+//app.use(cors());
+
+//method 2
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST', 'DELETE'],
+        allowedHeaders: ['Content-Type'],
+    })
+)
 
 app.get('/', (req, res) => {
     console.log(req);
